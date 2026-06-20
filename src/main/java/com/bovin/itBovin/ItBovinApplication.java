088ae3af8ccd4adc3e7a9fa79d4cf3d1b010a7d9
Giovanni Rambatoson <rggiovanni16@gmail.com>
2026-06-20 16:22:55 +0300

    mise en place des config

---
+++ src/main/java/com/bovin/itBovin/ItBovinApplication.java
@@ -0,0 +1,13 @@
+package com.bovin.itBovin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBovinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBovinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bovin/itBovin/ItBovinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bovin.itBovin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBovinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
