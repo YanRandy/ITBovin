369a22b767a4aaa102b88b21a772829ffa56c882
mahatoky <andrian.mahatoky@gmail.com>
2026-06-26 19:24:18 +0300

    feat : achat bovin

--- src/main/java/com/bovin/itBovin/ItBovinApplication.java
+++ src/main/java/com/bovin/itBovin/ItBovinApplication.java
@@ -9,5 +9,4 @@ public class ItBovinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItBovinApplication.class, args);
 	}
-
 }
--- src/main/java/com/bovin/itBovin/controller/AchatController.java
+++ src/main/java/com/bovin/itBovin/controller/AchatController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bovin.itBovin.dto.AchatBovinDto;
 import com.bovin.itBovin.model.FournisseurModel;
@@ -19,7 +20,7 @@ import com.bovin.itBovin.service.RaceService;
 @Controller
 @RequestMapping("/achat")
 public class AchatController {
-    
+
     @Autowired
     private FournisseurService fournisseurService;
 
@@ -30,23 +31,36 @@ public class AchatController {
     private AchatService achatService;
 
     @GetMapping("/bovin/{id_fournisseur}")
-    public String achatBovin(Model model,@PathVariable("id_fournisseur")  Integer idFournisseur) {
-        Optional<FournisseurModel> optionalFournisseur =  fournisseurService.findById(idFournisseur);
+    public String achatBovin(Model model, @PathVariable("id_fournisseur") Integer idFournisseur) {
+        Optional<FournisseurModel> optionalFournisseur = fournisseurService.findById(idFournisseur);
 
-        if(optionalFournisseur.isEmpty()) {
-            model.addAttribute("message","Fournisseur introuvable");
+        if (optionalFournisseur.isEmpty()) {
+            model.addAttribute("message", "Fournisseur introuvable");
             return "redirect:/fournisseur/list";
         } else {
-            model.addAttribute("fournisseur",optionalFournisseur.get());
-            model.addAttribute("races",raceService.getAllRaces());
+            model.addAttribute("fournisseur", optionalFournisseur.get());
+            model.addAttribute("races", raceService.getAllRaces());
             return "achat/bovin";
         }
     }
 
     @PostMapping("/bovin")
-    public String achatBovin(AchatBovinDto achatBovinDto) {
-        this.achatService.acheterBovin(achatBovinDto);
+    public String achatBovin(AchatBovinDto achatBovinDto,RedirectAttributes redirectAttributes) {
 
-        return "redirect:/fournisseur/list";
+        // verifier si le fournisseur existe
+        if (fournisseurService.findById(achatBovinDto.idFournisseur()).isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Fournisseur introuvable");
+            return "redirect:/fournisseur/list";
+        }
+
+        try {
+            this.achatService.acheterBovin(achatBovinDto);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "Une erreur s ' est produit lors de l achat de bovin : " + e.getMessage());
+            return "redirect:/achat/bovin/" + achatBovinDto.idFournisseur();
+        }
+
+        redirectAttributes.addFlashAttribute("message", "Bovin ajouter avec succes");
+        return "redirect:/achat/bovin/" + achatBovinDto.idFournisseur();
     }
 }
