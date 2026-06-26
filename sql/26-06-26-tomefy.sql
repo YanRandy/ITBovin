f535aadfcad83de425c0114501933905ad49934a
DirkTomefy <DirkTomefy@gmail.com>
2026-06-26 15:55:04 +0300

    [feat] : création d'une nouvelle table .sql

---
+++ sql/26-06-26-tomefy.sql
@@ -0,0 +1,8 @@
+CREATE TABLE caisse (
+    id SERIAL PRIMARY KEY,
+    libelle VARCHAR(100) NOT NULL,
+    solde_initial NUMERIC(12,2) DEFAULT 0
+);
+
+ALTER TABLE mouvement
+ADD COLUMN id_caisse INT REFERENCES caisse(id);
