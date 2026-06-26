1ac6836d9f3b139b5af4c2d243734f9e870a84ac
Sarubidy <rantsoavinatolotrasarobidy@gmail.com>
2026-06-26 20:28:55 +0300

    test interface

---
+++ src/main/java/com/bovin/itBovin/service/ClientServiceImpl.java
@@ -0,0 +1,31 @@
+package com.bovin.itBovin.service;
+
+import com.bovin.itBovin.model.ClientModel;
+import com.bovin.itBovin.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import java.util.Optional; // <-- AJOUTE CETTE LIGNE
+
+@Service
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public Page<ClientModel> getClients(String nom, String adresse, String contact, int page, int size, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return clientRepository.findAll(pageable);
+    }
+
+    // Ajoute cette implémentation :
+    @Override
+    public ClientModel getClientById(Integer id) {
+        return clientRepository.findById(id).orElse(null);
+    }
+}
