c79b4d1c2b9cb890a8a687e5285d29533843e2a7
DirkTomefy <DirkTomefy@gmail.com>
2026-06-26 17:09:13 +0300

    [fix] : fixer sql

--- sql/26-06-26-tomefy.sql
+++ sql/26-06-26-tomefy.sql
@@ -1,7 +1,8 @@
 CREATE TABLE caisse (
     id SERIAL PRIMARY KEY,
     libelle VARCHAR(100) NOT NULL,
-    solde_initial NUMERIC(12,2) DEFAULT 0
+    solde_initial NUMERIC(12,2) DEFAULT 0,
+    solde_actuelle NUMERIC(12,2) DEFAULT 0
 );
 
 ALTER TABLE mouvement
